d892f526c6405be41162634311cf0b98d9eb70aa
Tristan Hilbert <tchilbert@live.com>
2021-05-21 00:40:05 -0700

    Add Startup Process to Game

--- src/game.go
+++ src/game.go
@@ -1,21 +1,64 @@
 package main
 
-import "log"
+import (
+	"context"
+	"log"
+	"os/exec"
+	"time"
+)
+
+// Configurables
+const waitDurationForGame time.Duration = 10 * time.Second
+const commandToExec string = "node"
+
+var commandArgs []string = []string{"index.js", "--binding=5011"}
+
+// Global Variables | Singletons
+var commandContext context.Context = nil
+var cancelFunc func() = nil
 
 func startGameLogic() (func(), error) {
-	// 1. Execute Program
-	// 2. Init Pipe/Socket to Application
-	// 3. Init Shared Memory for efficient IPC
 	// 4. Construct Singleton Queue
 
-	// exec node example.js --binding=5011
-
-	log.Println("Game Logic is not implemented!")
+	executeCommand()
 	return cleanUpGameLogic, nil
 }
 
 func cleanUpGameLogic() {
 	log.Println("Cleaning Up Game Logic")
+	cancelFunc()
+
+	deadlineForStop := time.Now().Add(waitDurationForGame)
+	done := false
+	for deadlineForStop.Before(time.Now()) {
+		select {
+		case <-commandContext.Done():
+			done = true
+			break
+		default:
+		}
+	}
+
+	if !done {
+		log.Println("Game Did Not Finish Executing in Wait Time! Closing Program.")
+	}
+}
+
+func executeCommand() {
+	commandContext, cancelFunc = context.WithCancel(context.Background())
+
+	cmd := exec.CommandContext(commandContext, commandToExec, commandArgs...)
+
+	// Making sure to nil these for security reasons
+	cmd.Stdout = nil
+	cmd.Stderr = nil
+
+	go func() {
+		err := cmd.Run()
+		if err != nil {
+			log.Printf("Error Recieved From Game %v\n", err)
+		}
+	}()
 }
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
